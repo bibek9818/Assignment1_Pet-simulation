f42fffaf6bb2838e16a4e4d6a42c308bac00e405
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-02-02 01:26:45 -0500

    Functions and submenu created

--- Assignment1_Pet simulation/Program.cs
+++ Assignment1_Pet simulation/Program.cs
@@ -4,11 +4,41 @@ using System.Security.Cryptography.X509Certificates;
 class Program
 {
 
-    static void mainmenu()
+    //creating functions
+    private static object animal;
+
+
+    public void feed()
+    {
+
+    }
+
+
+    public void playing()
+    {
+
+    }
+
+
+    public void Resting()
+    { 
+
+    }
+
+
+    public void CheckBodyStatus()
+    {
+
+    }
+
+
+
+
+    static void MainUserDisplay()
     {
         Console.WriteLine("Main Menu\n");
         Console.WriteLine($"1.Feed ");
-        Console.WriteLine("2. Play with animal");
+        Console.WriteLine($"2. Play with {animal}");
         Console.WriteLine("3. Let animal rest ");
         Console.WriteLine("4. check animal status ");
         Console.WriteLine("5. Exit ");
@@ -52,6 +82,12 @@ class Program
         int result = Int32.Parse(firstoption);
 
 
+
+
+
+
+
+
         //If user inputs valuee 1, then Cat is selected
         if (result==1)
 
@@ -62,6 +98,30 @@ class Program
             Console.WriteLine($"You have named him {animal}!, let's take good care of him.");
             mainmenu();
         }
+
+
+//Making submenu
+
+
+        int submenu;
+
+        switch (submenu)
+        {
+            case 1:
+                Feed();
+                break;
+            case 2:
+            Playing();
+                break;
+            case 3:
+                Resting();
+                break;
+            case 4:
+                CheckBodyStatus();
+                return;
+        }
+
+
 
 
 
