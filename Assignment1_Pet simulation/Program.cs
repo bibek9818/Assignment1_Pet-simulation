e903a71aff4751d3e3eef1457caf4677f697230b
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-02-02 16:09:57 -0500

    Made new submenu.

--- Assignment1_Pet simulation/Program.cs
+++ Assignment1_Pet simulation/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
-class Program
+class VirtualFucntions
 {
+    public string PetType;
+    public string PetName;
+    public string Type { get; set; }
+    public string Name { get; set; }
+
+    public VirtualPet(string type, string name)
+    {
+        Type = type;
+        Name = name;
+       /* Hunger = 5;     // Initial values for hunger, happiness, and health
+        Happiness = 5;
+        Health = 5;*/
+    }
+
 
     //creating functions
     private static object animal;
 
 
-    public void feed()
+    public void feeding()
     {
 
     }
@@ -65,64 +80,138 @@ class Program
         }
     }
 
-    static void Main()
+    //Making submenu
+    public void submenu()
+    {
+        Console.WriteLine("Welcome to the Virtual Pet Simulator!");
+
+        while (true)
+        {
+           // DisplayStatus();
+
+            Console.WriteLine("Choose an action:");
+            Console.WriteLine("1. Feed the pet");
+            Console.WriteLine("2. Play with the pet");
+            Console.WriteLine("3. Rest the pet");
+            Console.WriteLine("4. Check the pet status");
+            Console.WriteLine("5. Exit");
+
+
+            string userInput = Console.ReadLine();
+            int mainmenuResult = Int32.Parse(userInput);
+
+            int choice;
+
+            if(mainmenuResult<1 || mainmenuResult>4)
+            {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.");
+            }
+            else
+            switch (mainmenuResult)
+            {
+                case 1:
+                    feeding();
+                    break;
+                case 2:
+                    playing();
+                    break;
+                case 3:
+                    Resting();
+                    break;
+                case 4:
+                        CheckBodyStatus();
+                    break;
+                default:
+                       
+                    return;
+                }
+
+        //    TimePasses();  Simulate the passage of time
+        }
+    }
+
+
+
+    public void PickVirtualPet()
     {
 
-
-        Console.WriteLine("***********Welcome to Virtual Pet Simulation**********");
-
-  
-       
-        Console.Write("1. Cat\n");
-        Console.Write("2. Dog\n");
-        Console.Write("3. Rabbit\n");
-
-       Console.Write("Choose the pet you want to raise by inputting the number\n\n");
+        Console.WriteLine("1. Cat\n");
+        Console.WriteLine("2. Dog\n");
+        Console.WriteLine("3. Rabbit\n");
+        Console.WriteLine("Choose the pet you want to raise by inputting the number\n\n");
         string firstoption = Console.ReadLine();
         int result = Int32.Parse(firstoption);
 
 
-
-
-
-
-
-
-        //If user inputs valuee 1, then Cat is selected
-        if (result==1)
-
-        { 
-            Console.Write("You've choosen Cat, what would you like to name it\n");
-            string animal = Console.ReadLine();
-
-            Console.WriteLine($"You have named him {animal}!, let's take good care of him.");
-            mainmenu();
-        }
-    
-//Making submenu
-
-
-        int submenu;
-
-        switch (submenu)
+        if(result>=1 && result<=3)
         {
-            case 1:
-                Feed();
-                break;
-            case 2:
-            Playing();
-                break;
-            case 3:
-                Resting();
-                break;
-            case 4:
-                CheckBodyStatus();
-                return;
+            switch(result)
+            {
+                case 1:
+                    type = "Cat";
+                    break;
+                case 2:
+                    type = "Dog";
+                    break;
+                case 3:
+                    Type = "Rabbit";
+                    break;
+
+            }
         }
 
 
+    }
+
+
+
+    public void mainmenu()
+    {
+        Console.WriteLine("***********Welcome to Virtual Pet Simulation**********");
+
+
+        PickVirtualPet();
+
+        Console.Write("Give your pet a name: ");
+        string petName = Console.ReadLine();
+
+    }
+
+
+
+}
+class Program
+{
+static void Main()
+{
+
+   
+
+    VirtualPet myPet = new VirtualPet(result, petName);
+
+
+
+
+
+
+
+
+
+
+
+    //If user inputs valuee 1, then Cat is selected
+    if (result == 1)
+
+    {
+        Console.Write("You've choosen Cat, what would you like to name it\n");
+        string animal = Console.ReadLine();
+
+        Console.WriteLine($"You have named him {animal}!, let's take good care of him.");
+        mainmenu();
+    }
 
 
 
     }
+
 }
